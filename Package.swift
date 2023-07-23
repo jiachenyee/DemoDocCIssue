2ae5067abfc484680515e1b35cf9409598850a9b
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-07-23 22:15:54 +0800

    update with tutorials, update url

--- Package.swift
+++ Package.swift
@@ -4,16 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "Tutorial",
+    name: "Tutorials",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Tutorial",
-            targets: ["Tutorial"]),
+            name: "Tutorials",
+            targets: ["Tutorials"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "Tutorial")
+        .target(name: "Tutorials")
     ]
 )
